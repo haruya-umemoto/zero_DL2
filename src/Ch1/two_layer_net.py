bbd65d32d60bb30979198387ffc0d1b1274bf421
Haruya Umemoto <umeco@HaruyanoMacBook-Pro.local>
2020-06-24 08:23:39 +0900

    create chapter folder

--- src/Ch1/two_layer_net.py
+++ src/Ch1/two_layer_net.py
@@ -1,14 +1,13 @@
 import numpy as np
-from activations import Sigmoid
-from layers import Affine
+from layers import Affine, Sigmoid, SoftmaxWithLoss
 
 class TwoLayerNet:
     def __init__(self, input_size, hidden_size, output_size):
         I, H, O = input_size, hidden_size, output_size
 
-        W1 = np.random.randn(H, I)
+        W1 = 0.01 * np.random.randn(I, H)
         b1 = np.random.randn(H)
-        W2 = np.random.randn(H, O)
+        W2 = 0.01 * np.random.randn(H, O)
         b2 = np.random.randn(O)
     
         self.layers = [
@@ -16,14 +15,25 @@ class TwoLayerNet:
             Sigmoid(),
             Affine(W2, b2)
         ]
+        self.loss_layer = SoftmaxWithLoss()
 
-        self.params = []
+        self.params, self.grads = [], []
         for layer in self.layers:
             self.params += layer.params
+            self.grads += layer.grads
     def predict(self, x):
         for layer in self.layers:
             x = layer.forward(x)
         return x
+    def forward(self, x, t):
+        score = self.predict(x)
+        loss = self.loss_layer.forward(score, t)
+        return loss
+    def backward(self, dout=1):
+        dout = self.loss_layer.backward(dout)
+        for layer in reversed(self.layers):
+            dout = layer.backward(dout)
+        return dout
 
 if __name__=='__main__':
     mdl = TwoLayerNet(4,4,1)
