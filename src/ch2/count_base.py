9d6aa2d8b7c64ddfaa9e702525c09be579988d32
Haruya Umemoto <umeco@HaruyanoMacBook-Pro.local>
2020-07-07 16:41:07 +0900

    complete chapter 2

--- src/ch2/count_base.py
+++ src/ch2/count_base.py
@@ -1,23 +1,6 @@
 import numpy as np
 from datasets import load_data
 
-def preprocess(text :str):
-    text = text.lower()
-    text = text.replace('.', ' .')
-    words = text.split(' ')
-
-    word_to_id = {}
-    id_to_word = {}
-    for word in words:
-        if word not in word_to_id:
-            new_id = len(word_to_id)
-            word_to_id[word] = new_id
-            id_to_word[new_id] = word
-    
-    corpus = np.array([word_to_id[w] for w in words])
-
-    return corpus, word_to_id, id_to_word
-
 def create_co_matrix(corpus, vocab_size, window_size=1):
     corpus_size = len(corpus)
     co_matrix = np.zeros((vocab_size, vocab_size), dtype=np.int32)
